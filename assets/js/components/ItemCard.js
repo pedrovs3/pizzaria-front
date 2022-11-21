8ef3009e932e66917ca8587e0fa6aae8828be06a
Pedro Vieira <pedrovs3@hotmail.com>
2022-11-21 10:32:43 -0300

    first commit

---
+++ assets/js/components/ItemCard.js
@@ -0,0 +1,168 @@
+class ItemCard extends HTMLElement {
+  constructor() {
+      super();
+      this.shadow = this.attachShadow({ mode: 'open' });
+      this.imageURL = 'https://images7.alphacoders.com/596/thumb-1920-596343.jpg';
+      this.title = '';
+      this.ingredients = '';
+      this.price = '';
+  }
+
+  connectedCallback() {
+    this.shadow.appendChild(this.component());
+    this.shadow.appendChild(this.styles());
+  }
+
+  component(){
+    const card = document.createElement('div');
+    card.classList.add('item-card');
+
+    const img = document.createElement('img');
+    img.classList.add('item-card__img');
+    img.setAttribute('src', this.imageURL);
+
+    const content = document.createElement('div');
+    content.classList.add('item-card__text-content');
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('item-card__info');
+
+    const title = document.createElement('h3');
+    title.classList.add('item-card__title');
+    title.textContent = this.title;
+
+    const ingredients = document.createElement('span');
+    ingredients.classList.add('item-card__ingredients');
+    ingredients.textContent = this.ingredients;
+
+    const price = document.createElement('div');
+    price.classList.add('item-card__price');
+
+    price.innerHTML = `R$${this.price.split(',')[0]},<span class="cents">${this.price.split(',')[1]}</span>`;
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('item-card__likeButton');
+
+    const infoGroup = document.createElement('div');
+
+    infoGroup.appendChild(title);
+    infoGroup.appendChild(ingredients);
+    content.appendChild(infoGroup)
+    content.appendChild(likeButton);
+
+    infoDiv.appendChild(img);
+    infoDiv.appendChild(content);
+
+    card.appendChild(infoDiv);
+    card.appendChild(price);
+
+    return card;
+  }
+
+  static get observedAttributes() {
+    return ['title', 'ingredients', 'price', 'image'];
+  }
+
+  attributeChangedCallback(nameAtr, oldValue, newValue) {
+    // this.nameAtr = newValue;
+
+    if (nameAtr === 'title') {
+      this.title = newValue;
+    } else if (nameAtr === 'ingredients') {
+      this.ingredients = newValue;
+    } else if (nameAtr === 'price'){
+      this.price = newValue;
+    }else if (nameAtr === 'image'){
+      this.imageURL = newValue;
+    }
+  }
+
+  styles() {
+    const styles = document.createElement('style');
+    styles.textContent = `
+      .item-card {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 20px;
+        max-width: 250px;
+        min-height: 300px;
+        color: white;
+        background-color: #3A3A3C;
+        text-align: left;
+        border-radius: 12px;
+      }
+
+      .item-card__img {
+        height: 130px;
+        width : 100%;
+        background-size: cover;
+        object-fit: cover;
+        background-position: center;
+        border-radius: 12px;
+      }
+
+      .item-card__info {
+        width: 100%;
+      }
+
+      .item-card__text-content {
+        display: grid;
+        width: 100%;
+        grid-template-columns: repeat(2, 1fr);
+        justify-content: center;
+      }
+
+      .item-card__title {
+        margin-bottom: 2px;
+        margin-top: 8px;
+        text-transform: capitalize;
+        gap: 0px
+        font-size: 1.5rem;
+        align-self: flex-start;
+      } 
+
+      .item-card__ingredients {
+        color: #AEAEB0;
+        align-self: flex-start;
+        font-size: 1rem;
+      }
+
+      .item-card__likeButton {
+        border: none;
+        margin-top: 8px;
+        justify-self: end;
+        width: 40px;
+        height: 35px;
+        background: url(assets/img/like.svg);
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+
+      .item-card__price {
+        width: 100%;
+        height: 70px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #F9C739;
+        border-radius: 50px;
+
+        font-size: 2rem;
+        font-weight: 600;
+        color: #000000;
+      }
+
+      .cents {
+        font-size: 28px;
+        transform: translateY(1px);
+      }
+    `;
+
+    return styles;
+  }
+}
+
+customElements.define('item-card', ItemCard);
