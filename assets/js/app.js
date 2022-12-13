fad89dcb10b0c8586deff59751ef5f66a354f071
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2022-12-12 23:46:49 -0300

    finishing the filters for products

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,12 +36,79 @@ const pizzaTypes = await pizzaTypesFetch();
 const pizzaStuffing = await pizzaStuffingFetch();
 
 export const populateCardapio = (data) => {
-  data.map((item) => {
-    const card = new ItemCard();
-    card.setTitle(item.name);
+  if (data[0].tbl_product) {
+    data.map((item) => {
+      console.log(item);
+      const card = new ItemCard();
+      card.setTitle(item.tbl_product.name);
 
-    if (item.tbl_pizza) {
-      const ingredients = item.tbl_pizza[0].tbl_pizza_ingredient;
+      if (item.tbl_pizza_ingredient) {
+        const ingredients = item.tbl_pizza_ingredient;
+        const ingredientsNames = [];
+        ingredients.forEach((ingredient) =>
+          ingredientsNames.push(ingredient.tbl_ingredient.name)
+        );
+
+        card.setIngredients(
+          ingredientsNames.join(", ").replace(/,\s([^,]+)$/, " e $1")
+        );
+      } else {
+        const { volume } = item;
+
+        card.setIngredients(`${volume} ml`);
+      }
+
+      const { tbl_picture } = item.tbl_product.tbl_product_pictures[0];
+
+      card.setPrice(item.tbl_product.price);
+      card.setImage(tbl_picture.picture_link);
+      card.setId(item.tbl_product.id);
+
+      card.connectedCallback();
+
+      cardapio.append(card.shadow);
+    });
+  } else {
+    data.map((item) => {
+      const card = new ItemCard();
+      card.setTitle(item.name);
+
+      if (item.tbl_pizza) {
+        const ingredients = item.tbl_pizza[0].tbl_pizza_ingredient;
+        const ingredientsNames = [];
+        ingredients.forEach((ingredient) =>
+          ingredientsNames.push(ingredient.tbl_ingredient.name)
+        );
+
+        card.setIngredients(
+          ingredientsNames.join(", ").replace(/,\s([^,]+)$/, " e $1")
+        );
+      } else {
+        const { volume } = item.tbl_drink[0];
+
+        card.setIngredients(`${volume} ml`);
+      }
+
+      const { tbl_picture } = item.tbl_product_pictures[0];
+
+      card.setPrice(item.price);
+      card.setImage(tbl_picture.picture_link);
+      card.setId(item.id);
+
+      card.connectedCallback();
+
+      cardapio.append(card.shadow);
+    });
+  }
+};
+
+export const populateProdutosEmPromocao = (data) => {
+  data.map((item) => {
+    console.log(item);
+    const card = new ItemCard();
+    card.setTitle(item.tbl_product.name);
+    if (item.tbl_product.tbl_pizza) {
+      const ingredients = item.tbl_product.tbl_pizza[0].tbl_pizza_ingredient;
       const ingredientsNames = [];
       ingredients.forEach((ingredient) =>
         ingredientsNames.push(ingredient.tbl_ingredient.name)
@@ -51,39 +118,10 @@ export const populateCardapio = (data) => {
         ingredientsNames.join(", ").replace(/,\s([^,]+)$/, " e $1")
       );
     } else {
-      const { volume } = item.tbl_drink[0];
+      const { volume } = item.tbl_product.tbl_drink[0];
 
       card.setIngredients(`${volume} ml`);
     }
-
-    const { tbl_picture } = item.tbl_product_pictures[0];
-
-    card.setPrice(item.price);
-    card.setImage(tbl_picture.picture_link);
-    card.setId(item.id);
-
-    card.connectedCallback();
-
-    cardapio.append(card.shadow);
-  });
-};
-
-export const populateProdutosEmPromocao = (data) => {
-  data.map((item) => {
-    console.log(item);
-    const card = new ItemCard();
-    card.setTitle(item.tbl_product.name);
-    const ingredients = item.tbl_product.tbl_pizza[0].tbl_pizza_ingredient;
-
-    let ingredientsNames = [];
-
-    ingredients
-      .slice(0, 1)
-      .forEach((ingredient) =>
-        ingredientsNames.push(ingredient.tbl_ingredient.name)
-      );
-
-    card.setIngredients(ingredientsNames.join(" "));
 
     const { tbl_picture } = item.tbl_product.tbl_product_pictures[0];
 
@@ -140,7 +178,7 @@ export const clearFilters = () => {
 const populateCategoryFilter = (data) => {
   data.payload.map((item) => {
     const option = document.createElement("option");
-    option.value = item.name;
+    option.value = item.id;
     option.text = item.name;
     categorySelect.appendChild(option);
   });
@@ -183,7 +221,7 @@ selects[0].addEventListener("change", (e) => {
 
   clearFilters();
 
-  if (value.toLowerCase() === "pizza") {
+  if (value == 1) {
     populatePizzaTypeFilter(pizzaTypes);
     populatePizzaStuffingFilter(pizzaStuffing);
   } else {
--- assets/js/handleFilters.js
+++ assets/js/handleFilters.js
@@ -1,15 +1,25 @@
 import { populateCardapio } from "./app.js";
 import fetchProducts from "./utils/fetchProducts.js";
-import { filterForCategory } from "./utils/fetchToFilterProducts.js";
+import {
+  filterForCategory,
+  filterForDrinkType,
+  filterForPizzaStuffing,
+  filterForPizzaType,
+  filterForPizzaTypeAndStuffing,
+} from "./utils/fetchToFilterProducts.js";
 
-const categorySelect = document.querySelectorAll(".select-input__option")[0];
+const selects = document.querySelectorAll(".select-input__option");
+const categorySelect = selects[0];
+const typeFilter = selects[1];
+const stuffingFilter = selects[2];
 
 console.log(categorySelect);
+
 categorySelect.addEventListener("change", async (e) => {
   e.preventDefault();
 
   document.querySelector(".grid-foods").innerHTML = "";
-  
+
   const { target } = e;
   const { value } = target;
 
@@ -22,7 +32,66 @@ categorySelect.addEventListener("change", async (e) => {
 
   const data = await filterForCategory(value);
 
-  console.log(data);
-
   populateCardapio(data);
+});
+
+typeFilter.addEventListener("change", async (e) => {
+  e.preventDefault();
+  document.querySelector(".grid-foods").innerHTML = "";
+
+  const { target } = e;
+  const { value } = target;
+
+  // pizza
+  if (categorySelect.value == 1) {
+    if (value.toLowerCase().includes("any")) {
+      // DEFAULT
+      const data = await filterForCategory(1);
+      populateCardapio(data);
+      return;
+    }
+    const data = await filterForPizzaType(value);
+    console.log(data);
+    populateCardapio(data.payload);
+  }
+
+  if (value.toLowerCase().includes("any")) {
+    // DEFAULT
+    const data = await filterForCategory(2);
+    populateCardapio(data);
+    return;
+  }
+
+  const data = await filterForDrinkType(value);
+
+  populateCardapio(data.payload);
+});
+
+stuffingFilter.addEventListener("change", async (e) => {
+  document.querySelector(".grid-foods").innerHTML = "";
+
+  e.preventDefault();
+  const { target } = e;
+  const { value } = target;
+  if (typeFilter.value.toLowerCase().includes("any")) {
+    if (value.toLowerCase().includes("any")) {
+      // DEFAULT
+      const data = await filterForCategory(1);
+      populateCardapio(data);
+      return;
+    }
+    const data = await filterForPizzaStuffing(value);
+
+    populateCardapio(data.payload);
+  } else {
+    if (value.toLowerCase().includes("any")) {
+      // DEFAULT
+      const data = await filterForCategory(1);
+      populateCardapio(data);
+      return;
+    }
+    const data = await filterForPizzaTypeAndStuffing(typeFilter.value, value);
+
+    populateCardapio(data.payload);
+  }
 });
