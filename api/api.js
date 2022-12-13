0af08117b91cebd7b17805073602ec5f82ec878e
Pedro Vieira <pedrovs3@hotmail.com>
2022-12-13 11:13:31 -0300

    Fixing url for Api

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/.netlify/functions/server', // 'https://pizza-cms-api.netlify.app/.netlify/functions/server'
+  baseURL: 'https://pizza-cms-api.netlify.app/.netlify/functions/server', // 'http://localhost:3333/.netlify/functions/server'
   headers: {
     common: {
       Authorization: `Bearer ${localStorage.getItem('access_token')}`,
